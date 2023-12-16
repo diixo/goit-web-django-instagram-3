81caf123a3686f54562962a4c0440abc33e70754
diixo <anatolii.podoba@gmail.com>
2023-12-16 02:01:59 +0200

    added decorator for logout_view

--- killer_instagram/users/views.py
+++ killer_instagram/users/views.py
@@ -2,9 +2,11 @@ from django.shortcuts import render, redirect
 from django.views import View
 from django.contrib import messages
 from django.contrib.auth import logout
+from django.contrib.auth.decorators import login_required
 
 from .forms import RegisterForm
 
+@login_required
 def logout_view(request):
     if request.method == 'GET':
         username = request.user.username
